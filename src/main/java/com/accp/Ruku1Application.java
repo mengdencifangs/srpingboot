58b15580914061e08309a2ac9fc889b726e6773a
Chen Li Min <2445356887@qq.com>
2018-10-17 11:05:44 +0800

    第二次提交

--- src/main/java/com/accp/Ruku1Application.java
+++ src/main/java/com/accp/Ruku1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Ruku1Application {
 
 	public static void main(String[] args) {
-		System.out.println("666");
+		System.out.println("7777");
 		SpringApplication.run(Ruku1Application.class, args);
 	}
 }
