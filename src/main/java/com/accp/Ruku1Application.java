7fc60b28cbbdcac46f72341c795dd3018a96f9d6
Chen Li Min <2445356887@qq.com>
2018-10-17 10:40:59 +0800

    第一次eclipse提交

---
+++ src/main/java/com/accp/Ruku1Application.java
@@ -0,0 +1,12 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ruku1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ruku1Application.class, args);
+	}
+}
